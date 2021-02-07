24a50cca6ab2684930ea060dbd8124d13c9167d5
Marcelo Oliveira <marcelo_alfahot@hotmail.com>
2021-02-07 17:47:43 -0300

    verify token

--- src/shared/infra/middlewares/authentication.ts
+++ src/shared/infra/middlewares/authentication.ts
@@ -1,1 +1,30 @@
-import {} from 'jsonwebtoken';
+import { verify, decode } from 'jsonwebtoken';
+import { Request, Response, NextFunction } from 'express';
+
+export default function verifyToken(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): NextFunction | Response | undefined {
+  const bearerToken = req.header('Authorization');
+  if (!bearerToken) return res.status(401).json('Access Denied');
+
+  try {
+    const secret: string =
+      process.env.JWTSECRET === undefined ? '' : process.env.JWTSECRET;
+    verify(bearerToken.replace('Bearer ', ''), secret);
+
+    const obj = decode(bearerToken.replace('Bearer ', ''));
+
+    if (obj !== null) {
+      req.user = {
+        type: obj['type'],
+        user_id: obj['user_id']
+      };
+    }
+
+    next();
+  } catch {
+    return res.status(401).json('Invalid token');
+  }
+}
