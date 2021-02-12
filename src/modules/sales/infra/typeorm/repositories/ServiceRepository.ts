b76d98210fd794304f0c340e2edb3dc5718262d2
Guilherme <luiz.guilherme@sambatech.com.br>
2021-02-11 21:34:24 -0300

    ajuste de nomes

--- src/modules/sales/infra/typeorm/repositories/ServiceRepository.ts
+++ src/modules/sales/infra/typeorm/repositories/ServiceRepository.ts
@@ -11,14 +11,14 @@ export default class ServiceRepository implements IServiceRepository {
   }
 
   public async create(data: ICreateServicesDTO): Promise<number> {
-    const userInserted = await this.ormRepository.insert(data);
-    const user_id = userInserted.identifiers[0].user_id;
-    return user_id;
+    const serviceInserted = await this.ormRepository.insert(data);
+    const service_id = serviceInserted.identifiers[0].user_id;
+    return service_id;
   }
 
   public async findOne(id: number): Promise<Service | undefined> {
-    const user = await this.ormRepository.findOne(id);
+    const service = await this.ormRepository.findOne(id);
 
-    return user;
+    return service;
   }
 }
