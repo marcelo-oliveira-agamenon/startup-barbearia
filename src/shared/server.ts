99886f6078618553f63ff1730a1f4c5b76adccd9
Guilherme <luiz.guilherme@sambatech.com.br>
2021-02-22 06:06:38 -0300

    Ajuste do server

--- src/shared/server.ts
+++ src/shared/server.ts
@@ -2,26 +2,16 @@ import 'reflect-metadata';
 import 'dotenv/config';
 import 'shared/container';
 import 'express-async-errors';
-import express, { Request, Response, NextFunction } from 'express';
+import express from 'express';
 import Routes from '@shared/infra/routes/index';
 import config from '@shared/infra/typeorm/ormconfig';
 import { createConnection } from 'typeorm';
 import { errors } from 'celebrate';
 import cors from 'cors';
-import AppError from '@shared/errors/AppError';
 import authentication from '@shared/infra/middlewares/authentication';
+import authExceptions from '@shared/infra/middlewares/authExceptions';
+import errorHandling from '@shared/infra/middlewares/errorHandling';
 
-function maybe(auth: Function) {
-  return function (request: Request, response: Response, next: NextFunction) {
-    const signInEndPoint =
-      request.path === '/users/' && request.method === 'POST';
-    if (signInEndPoint) {
-      next();
-    } else {
-      auth(request, response, next);
-    }
-  };
-}
 (async () => {
   try {
     await createConnection(config);
@@ -31,26 +21,13 @@ function maybe(auth: Function) {
   }
 
   const app = express();
-  app.use(cors(), express.json(), maybe(authentication), Routes, errors());
-
   app.use(
-    (
-      error: Error,
-      request: Request,
-      response: Response,
-      next: NextFunction
-    ) => {
-      if (error instanceof AppError) {
-        return response
-          .status(error.status)
-          .json({ status: 'Client error', message: error.description });
-      }
-      console.error(error);
-      return response.status(500).json({
-        status: 'Server error',
-        message: 'Internal server error'
-      });
-    }
+    cors(),
+    express.json(),
+    authExceptions(authentication),
+    Routes,
+    errors(),
+    errorHandling
   );
 
   app.listen(process.env.PORT, () => {
