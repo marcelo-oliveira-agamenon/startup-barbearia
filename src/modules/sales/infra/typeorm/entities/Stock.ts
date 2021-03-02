b39436efb6774cfd3fdc76560ca4e619aa387ea0
FilipeNasci <filipe.n.silva@hotmail.com>
2021-03-02 20:45:49 -0300

    stock entity

---
+++ src/modules/sales/infra/typeorm/entities/Stock.ts
@@ -0,0 +1,28 @@
+import {
+    Column,
+    CreateDateColumn,
+    DeleteDateColumn,
+    Entity,
+    OneToMany,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+    JoinColumn
+  } from 'typeorm';
+import Product from './Product';
+
+@Entity('stock')
+export class Stock {
+    @PrimaryGeneratedColumn('uuid')
+    stock_id: string;
+
+    @Column({
+        default: 0
+        })
+    quantity: number;
+
+    @OneToMany(() => Product, product => product.product_id)
+    products: Product[];
+
+    @CreateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
+    created_at: Date;
+}
