577e165a7646cc146110f3f96acd78ad4ce4927c
Guilherme <luiz.guilherme@sambatech.com.br>
2021-03-07 14:53:11 -0300

    refactor: migration

--- src/shared/infra/typeorm/migrations/1615139552729-Sale.ts
+++ src/shared/infra/typeorm/migrations/1615139552729-Sale.ts
@@ -1,13 +1,12 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class Sale1615137546945 implements MigrationInterface {
-  name = 'Sale1615137546945';
+export class Sale1615139552729 implements MigrationInterface {
+  name = 'Sale1615139552729';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       `CREATE TABLE "sale" ("sale_id" uuid NOT NULL DEFAULT uuid_generate_v4(), "value" numeric(10,2) NOT NULL DEFAULT '0', "discount" numeric(10,2) NOT NULL DEFAULT '0', "is_discount_fixed" boolean NOT NULL DEFAULT true, "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP, "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "deleted_at" TIMESTAMP WITH TIME ZONE, "clientClientId" uuid, "userUserId" uuid, CONSTRAINT "PK_1f5c38463e847a2a0e74cf8035a" PRIMARY KEY ("sale_id"))`
     );
-
     await queryRunner.query(
       `ALTER TABLE "sale" ADD CONSTRAINT "FK_30a80b84da4bd5c6fa4131035ff" FOREIGN KEY ("clientClientId") REFERENCES "client"("client_id") ON DELETE NO ACTION ON UPDATE NO ACTION`
     );
