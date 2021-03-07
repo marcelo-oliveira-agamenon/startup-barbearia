207c984b0b5e606c30724577f8f688ac283454f5
Guilherme <luiz.guilherme@sambatech.com.br>
2021-03-07 14:08:43 -0300

    refactor: rename sale class

--- src/modules/sales/infra/typeorm/entities/Sale.ts
+++ src/modules/sales/infra/typeorm/entities/Sale.ts
@@ -3,20 +3,35 @@ import {
   CreateDateColumn,
   DeleteDateColumn,
   Entity,
+  ManyToOne,
   PrimaryGeneratedColumn,
   UpdateDateColumn
 } from 'typeorm';
 
-@Entity('service')
-export default class Service {
-  @PrimaryGeneratedColumn('increment')
+import Client from '@modules/users/infra/typeorm/entities/Client';
+import { User } from '@modules/users/infra/typeorm/entities/User';
+
+@Entity('sale')
+export default class Sale {
+  @PrimaryGeneratedColumn('uuid')
   sale_id: number;
 
-  @Column()
-  client_id: string;
+  @ManyToOne(() => Client, (client) => client.sales)
+  client: Client;
+
+  @ManyToOne(() => User, (user) => user.sales)
+  user: User;
+
+  @Column({
+    default: 0,
+    type: 'numeric',
+    precision: 10,
+    scale: 2
+  })
+  discount: number;
 
   @Column()
-  user_id: number;
+  is_discount_fixed: boolean;
 
   @CreateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
   created_at: Date;
