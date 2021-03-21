6ba194f81567bd1de25029fe39e0d716a43db9a7
Guilherme <luiz.guilherme@sambatech.com.br>
2021-03-21 13:48:41 -0300

    refactor: server responsabilities

--- src/shared/server.ts
+++ src/shared/server.ts
@@ -5,33 +5,10 @@ import 'shared/container';
 import 'express-async-errors';
 import { createConnection } from 'typeorm';
 import config from '@shared/infra/typeorm/ormconfig';
+import app from '@shared/infra/config/app';
 
 createConnection(config)
   .then(async () => {
-    const express = (await import('express')).default;
-    const cors = (await import('cors')).default;
-    const routes = (await import('@shared/infra/routes/index')).default;
-    const errorHandling = (
-      await import('@shared/infra/middlewares/errorHandling')
-    ).default;
-    const authentication = (
-      await import('@shared/infra/middlewares/authentication')
-    ).default;
-    const authExceptions = (
-      await import('@shared/infra/middlewares/authExceptions')
-    ).default;
-    const { errors } = await import('celebrate');
-
-    const app = express();
-
-    app.use(
-      cors(),
-      express.json(),
-      authExceptions(authentication),
-      routes,
-      errorHandling,
-      errors()
-    );
     app.listen(process.env.PORT, () => {
       console.log('Server listening on PORT:', process.env.PORT);
     });
