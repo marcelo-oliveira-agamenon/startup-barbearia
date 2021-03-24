a10cc8b0c97a96af34929c33278ad70aa5a9dfee
Guilherme <luiz.guilherme@sambatech.com.br>
2021-03-24 08:05:55 -0300

    refactor: adjust stock imports

--- src/modules/sales/infra/http/controllers/StockController.ts
+++ src/modules/sales/infra/http/controllers/StockController.ts
@@ -1,11 +1,14 @@
 import { container } from 'tsyringe';
 import { Request, Response } from 'express';
 import { classToClass } from 'class-transformer';
-import { CreateStockService } from '@modules/sales/services/stock/CreateStockService';
-import { GetStockService } from '@modules/sales/services/stock/GetStockService';
-import { ListStocksService } from '@modules/sales/services/stock/ListStocksService';
-import { UpdateStockService } from '@modules/sales/services/stock/UpdateStockService';
-import { DeleteStockService } from '@modules/sales/services/stock/DeleteStockService';
+
+import {
+  CreateStockService,
+  GetStockService,
+  ListStocksService,
+  DeleteStockService,
+  UpdateStockService
+} from '@modules/sales/services/stock';
 
 export default class StockController {
   public async create(request: Request, response: Response): Promise<Response> {
@@ -33,7 +36,7 @@ export default class StockController {
     return response.status(200).json(stocks);
   }
 
-   public async update(request: Request, response: Response): Promise<Response> {
+  public async update(request: Request, response: Response): Promise<Response> {
     const data = request.body;
     const { stock_id } = request.params;
 
