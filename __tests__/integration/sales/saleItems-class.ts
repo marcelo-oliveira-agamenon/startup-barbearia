39244a99ed5995f466eb97107346e26193c5aef5
Guilherme <luiz.guilherme@sambatech.com.br>
2021-03-27 16:46:14 -0300

    test: sale items test

---
+++ __tests__/integration/sales/saleItems-class.ts
@@ -0,0 +1,120 @@
+import faker from 'faker';
+
+class SaleItemsClass {
+  product_id: string;
+  sale_id: string;
+  service_id: string;
+  quantity: number;
+  value: number;
+  limit: number;
+  offset: number;
+
+  constructor() {
+    this.value = faker.random.number();
+    this.limit = 3;
+    this.offset = 1;
+  }
+
+  get createRequestWithProduct() {
+    return {
+      sale_id: this.sale_id,
+      product_id: this.product_id,
+      quantity: this.quantity,
+      value: this.value
+    };
+  }
+  get createResponseWithProduct() {
+    const sale_items_id = expect.anything(),
+      service_id = expect.anything(),
+      created_at = expect.any(Date),
+      updated_at = expect.any(Date),
+      deleted_at = null;
+
+    return {
+      sale_items_id,
+      service_id,
+      created_at,
+      updated_at,
+      deleted_at,
+      sale_id: this.sale_id,
+      product_id: this.product_id,
+      value: this.value,
+      quantity: this.quantity
+    };
+  }
+  // get listRequest() {
+  //   return { limit: this.limit, offset: this.offset };
+  // }
+
+  // get getResponse() {
+  //   const sale_id = expect.anything(),
+  //     created_at = expect.anything(),
+  //     updated_at = expect.anything(),
+  //     value = expect.anything(),
+  //     discount = expect.anything(),
+  //     deleted_at = null;
+
+  //   return {
+  //     client_id: this.client_id,
+  //     user_id: this.user_id,
+  //     value,
+  //     discount,
+  //     is_discount_fixed: this.is_discount_fixed,
+  //     sale_id,
+  //     created_at,
+  //     updated_at,
+  //     deleted_at
+  //   };
+  // }
+
+  // get updateRequest() {
+  //   return {
+  //     user_id: this.user_id,
+  //     client_id: this.client_id,
+  //     discount: this.discount,
+  //     is_discount_fixed: this.is_discount_fixed
+  //   };
+  // }
+  // get updateResponse() {
+  //   const sale_id = expect.anything(),
+  //     created_at = expect.anything(),
+  //     updated_at = expect.anything(),
+  //     deleted_at = null,
+  //     discount = expect.anything(),
+  //     value = expect.anything();
+
+  //   return {
+  //     client_id: this.client_id,
+  //     user_id: this.user_id,
+  //     is_discount_fixed: this.is_discount_fixed,
+  //     value,
+  //     discount,
+  //     sale_id,
+  //     created_at,
+  //     updated_at,
+  //     deleted_at
+  //   };
+  // }
+
+  // get deleteResponse() {
+  //   const sale_id = expect.anything(),
+  //     created_at = expect.anything(),
+  //     updated_at = expect.anything(),
+  //     deleted_at = expect.anything(),
+  //     discount = expect.anything(),
+  //     value = expect.anything();
+
+  //   return {
+  //     client_id: this.client_id,
+  //     user_id: this.user_id,
+  //     is_discount_fixed: this.is_discount_fixed,
+  //     value,
+  //     discount,
+  //     sale_id,
+  //     created_at,
+  //     updated_at,
+  //     deleted_at
+  //   };
+  //}
+}
+export default SaleItemsClass;
---
+++ __tests__/integration/sales/saleitems.endPoint.test.ts
@@ -0,0 +1,140 @@
+import { container } from 'tsyringe';
+import { Connection, createConnection } from 'typeorm';
+
+import config from '@shared/infra/typeorm/ormconfig';
+import request from 'supertest';
+
+import SaleClass from './sale-class';
+import ProductClass from './product-class';
+import ServiceClass from './service-class';
+import SaleItemsClass from './saleItems-class';
+
+import { CreateSaleService } from '@modules/sales/services/sale/CreateSaleService';
+import { CreateProductService } from '@modules/sales/services/product/CreateProductService';
+import { CreateServiceService } from '@modules/sales/services/service/CreateServiceService';
+
+import SaleItems from '@modules/sales/infra/typeorm/entities/SaleItems';
+
+import app from '@shared/infra/config/app';
+
+let connection: Connection;
+
+const saleClass = new SaleClass();
+const productClass = new ProductClass();
+const serviceClass = new ServiceClass();
+const saleItemsClass = new SaleItemsClass();
+
+const TOKEN = `Bearer ${process.env.TOKEN}`;
+
+const createEndPoint = '/sale-items/signup',
+  listEndPoint = '/sale-items/';
+const commonEndPoint = '/sale-items/';
+
+describe('POST/GET/DELETE /sale-items/', function () {
+  beforeAll(async () => {
+    connection = await createConnection(config);
+
+    const CreateSale = container.resolve(CreateSaleService);
+    const createProduct = container.resolve(CreateProductService);
+    const createService = container.resolve(CreateServiceService);
+
+    const sale = await CreateSale.execute(saleClass);
+    const product = await createProduct.execute(productClass);
+    const service = await createService.execute(serviceClass);
+
+    if (sale) saleItemsClass.sale_id = sale.sale_id;
+    if (product) saleItemsClass.product_id = product.product_id;
+    if (service) saleItemsClass.service_id = service.service_id;
+  });
+  afterAll(async () => {
+    await connection.close();
+  });
+  it('Should create a sale with all input fields and return {sale}.', function (done) {
+    request(app)
+      .post(createEndPoint)
+      .set('Authorization', TOKEN)
+      .send(saleItemsClass.createRequestWithProduct)
+      .expect('Content-Type', /json/)
+      //   .expect(SaleItems)
+      .expect(201)
+      .expect((res) => {
+        // commonEndPoint += res.body.sale_id;
+        expect(res.body).toEqual(
+          expect.objectContaining(saleItemsClass.createResponseWithProduct)
+        );
+      })
+      .end(done);
+  });
+  // it('Should get a sale and return {sale}.', function (done) {
+  //   request(app)
+  //     .get(commonEndPoint)
+  //     .set('Authorization', TOKEN)
+  //     .expect('Content-Type', /json/)
+  //     .expect(Sale)
+  //     .expect(200)
+  //     .expect((res) => {
+  //       expect(res.body).toEqual(
+  //         expect.objectContaining(saleClass.getResponse)
+  //       );
+  //     })
+  //     .end(done);
+  // });
+
+  // it('Should list sales and return [{sale}].', function (done) {
+  //   request(app)
+  //     .get(listEndPoint)
+  //     .set('Authorization', TOKEN)
+  //     .query(saleClass.listRequest)
+  //     .expect('Content-Type', /json/)
+  //     .expect(Sale)
+  //     .expect(200)
+  //     .expect((res) => {
+  //       if (res.body.length) {
+  //         const firstElement = res.body[0];
+  //         expect(firstElement).toHaveProperty('client_id');
+  //         expect(firstElement).toHaveProperty('user_id');
+  //         expect(firstElement).toHaveProperty('value');
+  //         expect(firstElement).toHaveProperty('discount');
+  //         expect(firstElement).toHaveProperty('is_discount_fixed');
+  //         expect(firstElement).toHaveProperty('sale_id');
+  //         expect(firstElement).toHaveProperty('created_at');
+  //         expect(firstElement).toHaveProperty('updated_at');
+  //         expect(firstElement).toHaveProperty('deleted_at');
+  //       } else {
+  //         expect(res.body).toEqual(expect.arrayContaining([]));
+  //       }
+  //     })
+  //     .end(done);
+  // });
+
+  // it('Should update a sale and return {sale}.', function (done) {
+  //   request(app)
+  //     .put(commonEndPoint)
+  //     .set('Authorization', TOKEN)
+  //     .send(saleClass.updateRequest)
+  //     .expect('Content-Type', /json/)
+  //     .expect(Sale)
+  //     .expect(200)
+  //     .expect((res) => {
+  //       expect(res.body).toEqual(
+  //         expect.objectContaining(saleClass.updateResponse)
+  //       );
+  //     })
+  //     .end(done);
+  // });
+
+  // it('Should delete a sale softly and return {sale}.', function (done) {
+  //   request(app)
+  //     .delete(commonEndPoint)
+  //     .set('Authorization', TOKEN)
+  //     .expect('Content-Type', /json/)
+  //     .expect(Sale)
+  //     .expect(200)
+  //     .expect((res) => {
+  //       expect(res.body).toEqual(
+  //         expect.objectContaining(saleClass.deleteResponse)
+  //       );
+  //     })
+  //     .end(done);
+  // });
+});
