3ce51241773cccee5cf4c2aa853f38067a539039
Guilherme <luiz.guilherme@sambatech.com.br>
2021-04-02 19:29:26 -0300

    feat: handling jsonwebtokenerror in errorhandling

--- src/shared/infra/middlewares/errorHandling.ts
+++ src/shared/infra/middlewares/errorHandling.ts
@@ -2,6 +2,7 @@
 import AppError from '@shared/errors/AppError';
 import { isCelebrateError } from 'celebrate';
 import { Request, Response, NextFunction } from 'express';
+import { JsonWebTokenError } from 'jsonwebtoken';
 import { QueryFailedError } from 'typeorm';
 
 export default function (
@@ -12,18 +13,22 @@ export default function (
   next: NextFunction
 ) {
   console.error(error);
-
   if (error instanceof AppError) {
     return response
       .status(error.status)
       .json({ status: 'Client error', message: error.description });
-  } else if (isCelebrateError(error)) {
-    next(error);
   } else if (error instanceof QueryFailedError) {
     return response.status(500).json({
       status: error.name,
       message: `Unexpected error: ${error.message}`
     });
+  } else if (error instanceof JsonWebTokenError) {
+    return response.status(401).json({
+      status: error.name,
+      message: `Unexpected error: ${error.message}`
+    });
+  } else if (isCelebrateError(error)) {
+    next(error);
   } else {
     return response.status(500).json({
       status: 'Server error',
